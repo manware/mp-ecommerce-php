fcf5931173639d107273beaa8e882ba75df02104
Fernando Cuadrado <fernando@estudioquadra.com>
2020-06-11 02:25:47 -0300

    espero el body

--- ipn.php
+++ ipn.php
@@ -1,4 +1,6 @@
 <?
 error_log("===========  POST  ============== ".print_r($_POST, true));
 error_log("===========  REQUEST  ============== ".print_r($_REQUEST, true));
+$entityBody = file_get_contents('php://input');
+error_log("===========  TODO  ============== ".print_r($entityBody, true));
 ?>
